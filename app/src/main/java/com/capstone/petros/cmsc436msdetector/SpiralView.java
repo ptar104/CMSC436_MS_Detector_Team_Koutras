85e7de3c528c6317f3feaea3adf5d5d732d117bb
Peter Koutras <pkoutras@terpmail.umd.edu>
2017-02-12 22:56:48 -0500

    Spiral Report Updates
    Spiral reports are now organized into their own folder in the gallery,
    under “Spiral Test Results.”
    Furthermore, the results bitmap had extra space beneath that that can
    be filled with metrics from the test, such as score, jumps, and time
    taken.

--- app/src/main/java/com/capstone/petros/cmsc436msdetector/SpiralView.java
+++ app/src/main/java/com/capstone/petros/cmsc436msdetector/SpiralView.java
@@ -3,12 +3,10 @@ package com.capstone.petros.cmsc436msdetector;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
-import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Path;
-import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
 import android.util.AttributeSet;
@@ -30,8 +28,8 @@ import java.util.UUID;
 
 public class SpiralView extends View {
 
-    private Canvas _offScreenCanvas = null;
-    private Bitmap _offScreenBitmap = null;
+    private Canvas _offScreenCanvas = null, _reportCanvas = null;
+    private Bitmap _offScreenBitmap = null, _reportBitmap = null;
     private Path currPath = new Path();
     private Paint touchPaint = new Paint();
 
@@ -72,6 +70,7 @@ public class SpiralView extends View {
         background.setAntiAlias(true);
         background.setColor(0xFFFFFFFF);
         _offScreenCanvas.drawRect(0,0,_offScreenCanvas.getWidth(),_offScreenCanvas.getHeight(), background);
+        _reportCanvas.drawRect(0,0,_offScreenCanvas.getWidth(),_offScreenCanvas.getHeight()+500, background);
 
         // Draw spiral/touch lines here.
         Paint paint = new Paint();
@@ -100,6 +99,8 @@ public class SpiralView extends View {
         }
         _offScreenCanvas.drawPath(path,shadowPaint);
         _offScreenCanvas.drawPath(path,paint);
+        _reportCanvas.drawPath(path,shadowPaint);
+        _reportCanvas.drawPath(path,paint);
     }
 
     @Override
@@ -108,7 +109,9 @@ public class SpiralView extends View {
 
         if(_offScreenBitmap == null){
             _offScreenBitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight(), Bitmap.Config.ARGB_8888);
+            _reportBitmap = Bitmap.createBitmap(canvas.getWidth(), canvas.getHeight()+500, Bitmap.Config.ARGB_8888);
             _offScreenCanvas = new Canvas(_offScreenBitmap);
+            _reportCanvas = new Canvas(_reportBitmap);
             initSpiral();
         }
 
@@ -136,7 +139,8 @@ public class SpiralView extends View {
                 break;
         }
 
-        _offScreenCanvas.drawPath(currPath,touchPaint);
+        _offScreenCanvas.drawPath(currPath, touchPaint);
+        _reportCanvas.drawPath(currPath, touchPaint);
         if(up){
             currPath = new Path();
         }
@@ -152,11 +156,44 @@ public class SpiralView extends View {
     // Taken from code in Jon Froehlich's CMSC434 class.
     public void saveTestToGallery(ContentResolver cr){
 
+        //Set up the report.
+
+        //Dividing line
+        Paint dividingLinePaint = new Paint();
+        dividingLinePaint.setStrokeWidth(6);
+        dividingLinePaint.setColor(0xFF00FF00);
+        dividingLinePaint.setStyle(Paint.Style.STROKE);
+        Path dividingLinePath = new Path();
+        dividingLinePath.moveTo(0,_offScreenCanvas.getHeight());
+        dividingLinePath.lineTo(_offScreenCanvas.getWidth(),_offScreenCanvas.getHeight());
+        _reportCanvas.drawPath(dividingLinePath,dividingLinePaint);
+
+        //Rectangle
+        Paint rectPaint = new Paint();
+        rectPaint.setStyle(Paint.Style.FILL);
+        rectPaint.setColor(0xFFFFFFFF);
+        _reportCanvas.drawRect(0,_offScreenCanvas.getHeight(),_offScreenCanvas.getWidth(),
+                _offScreenCanvas.getHeight()+500,rectPaint);
+
+        Paint paintText = new Paint();
+        paintText.setTextSize(30);
+
+        // Here is where we would add the stats, such as score or lifts, to the report bitmap.
+        // Just add _offScreenCanvas.getHeight() to every y value.
+        SimpleDateFormat date = new SimpleDateFormat("MM/dd/yyyy hh:mm a");
+        _reportCanvas.drawText("Time of test: "+date.format(new Date()), 0,_offScreenCanvas.getHeight()+200, paintText);
 
         String fName = UUID.randomUUID().toString() + ".png";
-        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), fName);
+        File folder = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "/Spiral Test Results");
+        if(!folder.exists()){
+            if(!folder.mkdirs()){
+                System.out.println("MOTHER FUCK!!!");
+            }
+        }
+
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), "/Spiral Test Results/"+fName);
         try {
-            _offScreenBitmap.compress(Bitmap.CompressFormat.PNG, 100, new FileOutputStream(file));
+            _reportBitmap.compress(Bitmap.CompressFormat.PNG, 100, new FileOutputStream(file));
 
             ContentValues values = new ContentValues();
 
