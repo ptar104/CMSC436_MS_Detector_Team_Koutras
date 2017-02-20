fff1937d4c666eb3f24a5675f95d0ea8d239dc13
Peter Koutras <pkoutras@terpmail.umd.edu>
2017-02-19 20:21:34 -0500

    Merged Jitter, orientation, and test code
    Hooked up the test code to the orientation sensor, and added the jitter
    detector code.

--- app/src/main/java/com/capstone/petros/cmsc436msdetector/BallActivity.java
+++ app/src/main/java/com/capstone/petros/cmsc436msdetector/BallActivity.java
@@ -4,11 +4,19 @@ import android.content.Context;
 import android.graphics.Color;
 import android.app.FragmentManager;
 import android.app.Activity;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.view.MotionEvent;
 import android.view.View;
 
 public class BallActivity extends Activity {
+
+    SensorEventListener sel;
+    SensorManager sensorManager;
+    Sensor accelerometer, magnetometer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,8 +26,50 @@ public class BallActivity extends Activity {
         FragmentManager fragmentManager = getFragmentManager();
         InstructionFragment frag = new InstructionFragment();
         frag.show(fragmentManager, null);
-        
 
+        sensorManager = (SensorManager) this.getSystemService(this.SENSOR_SERVICE);
+        accelerometer = sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        magnetometer = sensorManager.getDefaultSensor(Sensor.TYPE_MAGNETIC_FIELD);
+
+        sel = new SensorEventListener() {
+            float[] mGravity;
+            float[] mGeomagnetic;
+            public void onSensorChanged(SensorEvent event) {
+                if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER)
+                    mGravity = event.values;
+                if (event.sensor.getType() == Sensor.TYPE_MAGNETIC_FIELD)
+                    mGeomagnetic = event.values;
+                if (mGravity != null && mGeomagnetic != null) {
+                    float Ro[] = new float[9];
+                    float I[] = new float[9];
+                    boolean success = SensorManager.getRotationMatrix(Ro, I, mGravity, mGeomagnetic);
+                    if (success) {
+                        float orientation[] = new float[3];
+                        SensorManager.getOrientation(Ro, orientation);
+                        // Azimuth = orientation[0], pitch = [1], roll = [2]
+                        ((BallView)findViewById(R.id.ballView)).recieveSensorInput(
+                                orientation[2], orientation[1], orientation[0]);
+                    }
+                }
+            }
+
+            @Override
+            public void onAccuracyChanged(Sensor sensor, int accuracy) {/*cough*/}
+        };
+
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        sensorManager.registerListener(sel, accelerometer, SensorManager.SENSOR_DELAY_GAME);
+        sensorManager.registerListener(sel, magnetometer, SensorManager.SENSOR_DELAY_GAME);
+    }
+
+    @Override
+    protected void onStop(){
+        super.onStop();
+        sensorManager.unregisterListener(sel);
     }
 
 
--- app/src/main/java/com/capstone/petros/cmsc436msdetector/BallView.java
+++ app/src/main/java/com/capstone/petros/cmsc436msdetector/BallView.java
@@ -24,6 +24,15 @@ public class BallView extends View {
     private int x = (disp.getWidth() / 2);
     private int y = (disp.getHeight() / 2);
 
+    long samples = 0;
+    float currRoll = 0, currPitch = 0;
+    float prevRoll = 0, prevPitch = 0;
+    double totalScore = 0;
+    boolean first = true;
+
+    static final double MAGTHRESHOLD = 45.0, ANGLETHRESHOLD = 72.0; // From experience
+    double PITODEG = (360 / (2 * Math.PI));
+
     public BallView(Context context) {
         super(context);
     }
@@ -38,21 +47,95 @@ public class BallView extends View {
 
     }
 
+    public void recieveSensorInput(float roll, float pitch, float azimuth){
+        // Roll would change x...
+        // Pitch would change y...
+        currRoll = roll;
+        currPitch = pitch;
+
+        if(currRoll > Math.PI/2.0){
+            currRoll = (float)(Math.PI/2.0);
+        }
+        if(currRoll < -1*(Math.PI/2.0)){
+            currRoll = -1*(float)(Math.PI/2.0);
+        }
+        if(currPitch > (Math.PI/2.0)){
+            currPitch = (float)(Math.PI/2.0);
+        }
+        if(currPitch < -1*(Math.PI/2.0)){
+            currPitch = -1*(float)(Math.PI/2.0);
+        }
+
+        // If roll/pitch escape -90 to 90, phone waaaay too tilted.
+        invalidate();
+    }
+
     @Override
     public void onDraw(Canvas canvas) {
         super.onDraw(canvas);
         paint1.setColor(Color.WHITE);
-        paint2.setColor(Color.parseColor("#3BBBBB"));
+        paint1.setTextSize(30);
+        paint2.setColor(Color.BLACK);
         paint3.setColor(Color.WHITE);
         ball.setColor(Color.RED);
+
+        double xDeg = currRoll*PITODEG;
+        double yDeg = -1*currPitch*PITODEG;
+
+        // Parsing out slight degrees.
+        if(xDeg < 2 && xDeg > -2){
+            xDeg = 0;
+        }
+        if(yDeg < 2 && yDeg > -2){
+            yDeg = 0;
+        }
+
+        x += xDeg;
+        y += yDeg;
+
+        if(x < 0){
+            x = 0;
+        }
+        if(x > getWidth()){
+            x = getWidth();
+        }
+
+        if(y < (getHeight() / 2) - (getWidth() / 2)){
+            y = (getHeight() / 2) - (getWidth() / 2);
+        }
+        if(y > (getHeight() / 2) + (getWidth() / 2)){
+            y = (getHeight() / 2) + (getWidth() / 2);
+        }
+
+        double midX = getWidth() / 2, midY = getHeight() / 2;
+        double distance = Math.sqrt((1.0*(x-midX))*(1.0*(x-midX)) + (1.0*(y-midY))*(1.0*(y-midY)));
+        if(distance <= getWidth()/6){
+            paint3.setColor(Color.GREEN);
+        }
+        else if(distance <= getWidth()/3){
+            paint2.setColor(Color.GREEN);
+        }
+        else{
+            paint1.setColor(Color.GREEN);
+        }
+
+        if(distance >= getWidth()/2){
+            // Outside last circle.
+            double angle = Math.atan2(-1.0 * (y - (getHeight()/2.0)), x - (getWidth() / 2.0));
+            x = (int)(midX + (getWidth()/2.0) * Math.cos(angle));
+            y = (int)(midY - (getWidth()/2.0) * Math.sin(angle));
+        }
+        //canvas.drawText("Angle: " + angle, 10, getHeight()-100, paint1);
+
         canvas.drawCircle(getWidth()/2, getHeight()/2, getWidth()/2, paint1);
         canvas.drawCircle(getWidth()/2, getHeight()/2, getWidth()/3, paint2);
         canvas.drawCircle(getWidth()/2, getHeight()/2, getWidth()/6, paint3);
-        canvas.drawCircle(x, y, 20, ball);
-
+        canvas.drawCircle(x, y, 60, ball);
+        calculateJitteryness();
     }
 
     public boolean onTouchEvent(MotionEvent event) {
+        /*
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN:
 
@@ -64,12 +147,66 @@ public class BallView extends View {
                 y = (int) event.getY();
                 break;
         }
-        int width = getWidth()/2;
-        int height = getHeight()/2;
+        int width = getWidth() / 2;
+        int height = getHeight() / 2;
 
-        if(x >= width - width && y >= height - width && x <= width + width && y <= height + width){
+        if (x >= width - width && y >= height - width && x <= width + width && y <= height + width) {
             invalidate();
         }
+        */
         return true;
+    }
+
+    public void calculateJitteryness() {
+        // Idea: to get jitteryness, get an average of angle and magnitue of orientation changes
+        // over the points. Take the f-score (harmonic mean),
+        // so BOTH have to be high for jitter score to be high.
+        // Magnitude is between 0 and 90, based on how much the phone is tilted?
+        // Angle is between 0 and 180, based on the angle of the phone?
+        // May want the f-score much more based on magnitude than angle.
+        // As at low magnitude, high flucuation of angle...
+        if(!first) {
+            samples++;
+            double currAngle = Math.atan2(currPitch, currRoll);
+            // My phone's roll is, like, backwards, but that should be ok?
+            // It's about relative angles, anyway.
+            // Angle goes from 0 to pi, jumps to negative pi, back to 0...
+            // Want to smooth it so it can be between 0 and 360
+            if (currAngle < 0) {
+                currAngle += 2 * Math.PI; // Normalize
+            }
+            currAngle *= PITODEG; // Convert to 360.
+            double currMagnitude = Math.sqrt(currRoll * PITODEG * currRoll * PITODEG +
+                    currPitch * PITODEG * currPitch * PITODEG);
+
+            double prevAngle = Math.atan2(prevPitch, prevRoll);
+            if (prevAngle < 0) {
+                prevAngle += 2 * Math.PI; // Normalize
+            }
+            prevAngle *= PITODEG; // Convert to 360.
+            double prevMagnitude = Math.sqrt(prevRoll * PITODEG * prevRoll * PITODEG +
+                    prevPitch * PITODEG * prevPitch * PITODEG); //roll * pitch
+
+            double angleBetween = Math.min(Math.min(Math.abs(currAngle - prevAngle),
+                    Math.abs((currAngle + 360) - prevAngle)),
+                    Math.abs((currAngle - 360) - prevAngle));
+            angleBetween /= ANGLETHRESHOLD;
+            double averageMag = Math.abs(currMagnitude - prevMagnitude);
+            averageMag /= MAGTHRESHOLD;
+            double beta = 1;
+            double fscore = (1 + beta * beta) * ((angleBetween * averageMag) / ((beta * beta * averageMag) + angleBetween));
+            if(((beta * beta * averageMag) + angleBetween) == 0){
+                fscore = 0;
+            }
+            totalScore += fscore;
+        }
+
+        prevRoll = currRoll;
+        prevPitch = currPitch;
+        first = false;
+    }
+
+    public double getAverageFscore(){
+        return totalScore / (samples * 1.0);
     }
 }
