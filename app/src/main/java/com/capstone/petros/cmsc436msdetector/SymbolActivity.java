34f7fd8d25c82375834083f007fe17b8f5fd395e
bergeralex <alexfberger@gmail.com>
2017-04-25 21:33:38 -0400

    Layout and images for Symbol Activity
    Made the symbol key (hardcoded). it will need to be randomized.

---
+++ app/src/main/java/com/capstone/petros/cmsc436msdetector/SymbolActivity.java
@@ -0,0 +1,13 @@
+package com.capstone.petros.cmsc436msdetector;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SymbolActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_symbol);
+    }
+}
